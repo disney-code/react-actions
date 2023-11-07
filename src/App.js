5fac5301167a480e2efb4beb7eab346b5c19de25
CharleneTSJ <charlenetansijie@outlook.com>
2023-11-07 14:22:30 +0800

    today 10

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Charlene Tan Si JIE
+          Learn React Charlene Tan Si Jie
         </a>
       </header>
     </div>
